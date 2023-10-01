51ae75022459d97d047fa8ded8efc6494b7bd2c1
lucaguada <luca.guadagnini@gmail.com>
2023-10-01 22:08:22 +0200

    I don't actually remember

--- src/main/java/io/trydent/httpserver/Main.java
+++ src/main/java/io/trydent/httpserver/Main.java
@@ -6,23 +6,16 @@ import com.sun.net.httpserver.SimpleFileServer;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.nio.file.Path;
 
-import static java.util.Objects.requireNonNull;
-
 class Main {
-  public static void main(String... args) {
-    try {
-      var httpServer = HttpServer.create(new InetSocketAddress(8080), Integer.MAX_VALUE);
-      var resource = Main.class.getClassLoader().getResource("web/index.html");
-      var path = Path.of(Main.class.getClassLoader().getResource("web/index.html").toURI());
-      System.out.println(path.toUri());
-      httpServer.createContext("/", SimpleFileServer.createFileHandler(path.getParent()));
-      httpServer.start();
-      System.out.println("Http Server started on port 8080");
-    } catch (IOException | URISyntaxException e) {
-      throw new RuntimeException(e);
-    }
+  public static void main(String... args) throws IOException, URISyntaxException {
+    var httpServer = HttpServer.create(new InetSocketAddress(8080), Integer.MAX_VALUE);
+    var resource = Main.class.getClassLoader().getResource("web/index.html");
+    var path = Path.of(Main.class.getClassLoader().getResource("web/index.html").toURI());
+    System.out.println(path.toUri());
+    httpServer.createContext("/", SimpleFileServer.createFileHandler(path.getParent()));
+    httpServer.start();
+    System.out.println("Http Server started on port 8080");
   }
 }
