860d63ed3585801cb448d46c99ff519e02dd2d78
lucaguada <luca.guadagnini@gmail.com>
2023-10-10 16:19:18 +0200

    added pem and ca-cert loading

--- src/main/java/io/trydent/httpserver/Main.java
+++ src/main/java/io/trydent/httpserver/Main.java
@@ -1,22 +1,29 @@
 package io.trydent.httpserver;
 
-import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpsConfigurator;
+import com.sun.net.httpserver.HttpsServer;
 import com.sun.net.httpserver.SimpleFileServer;
-import org.bouncycastle.jce.ECNamedCurveTable;
-import org.bouncycastle.jce.spec.ECNamedCurveParameterSpec;
-import org.bouncycastle.jce.spec.ECNamedCurveSpec;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.openssl.PEMKeyPair;
+import org.bouncycastle.openssl.PEMParser;
+import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
 
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
-import java.security.KeyFactory;
-import java.security.NoSuchAlgorithmException;
+import java.security.*;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
 import java.security.interfaces.ECPrivateKey;
-import java.security.spec.ECPrivateKeySpec;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.PKCS8EncodedKeySpec;
-import java.util.Base64;
+import java.util.Optional;
+import java.util.concurrent.Executors;
 
 import static java.util.Objects.requireNonNull;
 
@@ -30,42 +37,75 @@ enum Main {
   private final String domainCrt = STR. "\{ path }cert/domain.alpenflow.io.crt" ;
   private final String domainCsr = STR. "\{ path }cert/domain.alpenflow.io.csr" ;
 
-  public ECPrivateKey privateKey() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
-    try (final var pemResource = Main.class.getClassLoader().getResourceAsStream(Instance.domainPem)) {
-      final var key = new String(requireNonNull(pemResource).readAllBytes());
+  public Optional<ECPrivateKey> privateKey() throws IOException {
+    try (
+      final var pemResource = Main.class.getClassLoader().getResourceAsStream(Instance.domainPem);
+      final var inputReader = new InputStreamReader(requireNonNull(pemResource));
+      final var pemParser = new PEMParser(inputReader)
+    ) {
+      final var parsedPem = pemParser.readObject();
 
-      System.out.println(key);
-
-      final var privateKeyPEM = key
-        .replace("-----BEGIN EC PRIVATE KEY-----", "")
-        .replaceAll(System.lineSeparator(), "")
-        .replace("-----END EC PRIVATE KEY-----", "");
-
-      final var decoded = Base64.getDecoder().decode(privateKeyPEM.getBytes());
-
-      final var params = ECNamedCurveTable.getParameterSpec("secp256k1");
-      final var curve = params.getCurve();
-      
-      return (ECPrivateKey) KeyFactory
-        .getInstance("EC")
-        .generatePrivate(new ECPrivateKeySpec(decoded));
+      if (parsedPem instanceof PEMKeyPair keyPair) {
+        final var privateKey = new JcaPEMKeyConverter().getPrivateKey(keyPair.getPrivateKeyInfo());
+        return privateKey instanceof ECPrivateKey it ? Optional.of(it) : Optional.empty();
+      }
     }
+    return Optional.empty();
   }
 
-  public void setup() throws IOException, URISyntaxException, NoSuchAlgorithmException, InvalidKeySpecException {
-    System.out.println(STR. "\{ privateKey().getEncoded() }" );
+  public Optional<Certificate> caCertificate() throws IOException, CertificateException {
+    try (
+      final var crtResource = Main.class.getClassLoader().getResourceAsStream(Instance.domainCrt);
+      final var inputReader = new InputStreamReader(requireNonNull(crtResource));
+      final var crtParser = new PEMParser(inputReader)
+    ) {
+      final var parsedCrt = crtParser.readObject();
 
-    var httpServer = HttpServer.create(new InetSocketAddress(8080), Integer.MAX_VALUE);
+      if (parsedCrt instanceof X509CertificateHolder holder) {
+        return Optional.ofNullable(new JcaX509CertificateConverter().getCertificate(holder));
+      }
+    }
+
+    return Optional.empty();
+  }
+
+  public void setup() throws IOException, URISyntaxException, NoSuchAlgorithmException, KeyStoreException, CertificateException, UnrecoverableKeyException, KeyManagementException {
+    Security.insertProviderAt(new BouncyCastleProvider(), 1);
+
+    final var caCertificate = caCertificate().orElseThrow();
+    final var privateKey = privateKey().orElseThrow();
+
+    final var caKeyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+    caKeyStore.load(null, null);
+    caKeyStore.setCertificateEntry("ca-certificate", caCertificate);
+
+    final var trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+    trustManagerFactory.init(caKeyStore);
+
+    final var clientKeyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+    clientKeyStore.load(null, null);
+    clientKeyStore.setCertificateEntry("certificate", caCertificate);
+    clientKeyStore.setKeyEntry("private-key", privateKey, "password".toCharArray(), new Certificate[]{caCertificate});
+
+    var keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+    keyManagerFactory.init(clientKeyStore, "password".toCharArray());
+
+    final var tls = SSLContext.getInstance("TLSv1.3");
+    tls.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), null);
+
+    var httpsServer = HttpsServer.create(new InetSocketAddress(8443), Integer.MAX_VALUE);
     var indexResource = Main.class.getClassLoader().getResource(Instance.indexHtml);
     var path = Path.of(requireNonNull(indexResource).toURI());
     System.out.println(path.toUri());
-    httpServer.createContext("/", SimpleFileServer.createFileHandler(path.getParent()));
-    httpServer.start();
-    System.out.println("Http Server started on port 8080");
+    httpsServer.setHttpsConfigurator(new HttpsConfigurator(tls));
+    httpsServer.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+    httpsServer.createContext("/", SimpleFileServer.createFileHandler(path.getParent()));
+    httpsServer.start();
+    System.out.println("Http Server started on port 8443");
 
   }
 
-  public static void main(String... args) throws IOException, URISyntaxException, NoSuchAlgorithmException, InvalidKeySpecException {
+  public static void main(String... args) throws IOException, URISyntaxException, NoSuchAlgorithmException, UnrecoverableKeyException, CertificateException, KeyStoreException, KeyManagementException {
     Instance.setup();
   }
 }
